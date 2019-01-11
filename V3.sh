25e7aafa6e373a1968bdeeda8326d94890e6af26
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2019-01-11 22:56:18 +0700

    Create V3.sh

---
+++ V3.sh
@@ -0,0 +1,631 @@
+#wa :083851312460
+#update 6 January 2019
+
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+
+figlet  welcome to the code xidploit maker | lolcat
+sleep 5
+echo "wait a second" | lolcat
+sleep 3
+ 
+clear
+echo "tunggu sebentar... /";
+sleep 0.1;
+clear
+echo 'Tunggu sebentar... -';
+sleep 0.1;
+clear
+echo 'tUnggu sebentar... \';
+sleep 0.1;
+clear
+echo "tuNggu sebentar... /";
+sleep 0.1;
+clear
+echo 'tunGgu sebentar... -';
+sleep 0.1;
+clear
+echo 'tungGu sebentar... \';
+sleep 0.1;
+clear
+echo "tunggU sebentar... /";
+sleep 0.1;
+clear
+echo "tunggu Sebentar... -";
+sleep 0.1;
+clear
+echo 'tunggu sEbentar... \';
+sleep 0.1;
+clear
+echo 'tunggu seBentar... -';
+sleep 0.1;
+clear
+echo "tunggu sebEntar... /";
+sleep 0.1;
+clear
+echo 'tunggu sebeNtar... -';
+sleep 0.1;
+clear
+echo 'tunggu sebenTar... \';
+sleep 0.1;
+clear
+echo "tunggu sebentAr... /";
+sleep 0.1;
+clear
+sleep 2
+toilet -f slant -F gay "T00LS XIDPLOIT" | lolcat
+echo "'\033[32;1m'"
+cal
+date
+
+echo                      " [•]===><===<>===><===<>===><===<>==><==[]"|lolcat
+echo                      " [•]Author   :  MR_XiD                  []"|lolcat
+echo                      " [•]Wa       :  083851312460            []"|lolcat
+echo                      " [•]thanks to: JATIM BLACKHAT           []"|lolcat
+echo                      " [•]Thansk to: L4EFY                    []"|lolcat
+echo                      " [•]thanks to: D35TR0Y SQUAD            []"|lolcat
+echo                      " [•]Note2    : Copyright© 2018          []"|lolcat
+echo                      " [•]===><==<>===><===<>===><===<>===><==[]"|lolcat
+                            
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(1. WEB HACK"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(2. BOM SPAM "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat     
+echo    " $-(3. HAMMER DDOS "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(4. SQL MAP"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(5. SPAM WHATSAPP"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(6. New Tool TUAN B4DUT "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(7. Tool TUAN B4DUT V3"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(8. TOOL Cyb3r gh0st "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(9. TOOL TU4N N1CKN4M3"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(10.MAKE SCRIP DEFACE "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(11. INFO ANDROID"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(12. KALI NETHUNTER"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤π¤π¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(13. CRACK PW HASH"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(14. BOT KOMEN FB"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(15. LOCALIZAR IP"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(16. Tools-B4J1N64Nv5"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(17. TEMBAK XL"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(18. HASH BUSTER"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(19. TOOL UBUNTU"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(20. SPAM BOOM MALL"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(21. MBF HACK FB"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(22. INSTALL WEBDAV"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(23. TOOL FSOCIETY"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(24. TOOL D-TECT"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(25. HEKER REQODOG"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(26. HACK WIFI (ROOT)"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(27. LACAK IP"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(28. TORS HAMMER"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(29. TOOL INSTALL"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(30. LAZIMUX"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " $-(00. EXIT"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "╭─[Coder Xidploit]"
+read -p "╰─root@./MR_XID====->" sf;
+
+if [ $sf = 1 ] || [ $sf = 1 ]
+then
+echo    "==========================="|lolcat
+echo    " 1. admin finder "
+echo    "==========================="|lolcat
+echo    " 2. weeman "
+echo    "===========================" | lolcat
+echo    " 3. Black hydra"
+echo    "===========================" | lolcat
+echo    " 4. crewbot"
+echo    "===========================" | lolcat
+echo    " 5. Red hawk "
+echo    "===========================" | lolcat
+echo    " 6. KK generate "
+echo    "===========================" | lolcat
+echo    " 00. back "
+echo    "===========================" | lolcat
+echo    "╭─[Coder Xidploit]"
+read -p "╰─root@./MR_XID==->>" fri;
+fi
+if [ $fri = 1 ] || [ $fri = 1 ]
+then
+clear 
+git clone https://github.com/Techzindia/admin_penal
+cd admin_penal
+chmod +x admin_panel_finder.py
+python2 admin_panel_finder.py
+fi 
+
+if [ $fri = 2 ] || [ $fri = 2 ]
+then 
+clear
+figlet "Weeman" | lolcat
+apt install php
+git clone https://github.com/evait-security/weeman.git
+mv weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $fri = 3 ] || [ $fri = 3 ]
+then
+clear 
+cowsay -f kiss.cow "HyDra" | lolcat
+figlet "HyDra" | lolcat
+git clone https://github.com/Gameye98/Black-Hydra.git
+mv Black-Hydra
+cd Black-Hydra
+python2 blackhydra.py
+fi
+
+if [ $fri = 4 ] || [ $fri = 4 ]
+then
+clear 
+git clone https://github.com/Xeit666h05t/CrewBot
+mv CrewBot
+cd CrewBot
+python2 CrewBot.py
+fi
+
+if [ $fri = 5 ] || [ $fri = 5 ]
+then 
+clear 
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $fri = 6 ] || [ $fri = 6 ]
+then
+clear
+figlet "KK Generate"
+git clone https://github.com/zerosvn/ktpkkgenerate
+cd ktpkkgenerate
+php zerosvn.php
+fi
+
+if  [ $fri = 00 ] || [ $fri = 00 ]
+then 
+clear 
+sh Comand.sh
+fi
+
+if [ $sf = 2 ] || [ $sf = 2 ]
+then 
+clear 
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sleep 1
+sh LITESPAM.sh
+echo "langsung santet mantan :v" | lolcat
+fi
+
+if [ $sf = 3 ] || [ $sf = 3 ]
+then 
+clear
+git clone https://github.com/cyweb/hammer 
+cd hammer 
+chmod 7777 hammer.py
+sleep 1
+python hammer.py 
+fi
+
+if [ $sf = 4 ] || [ $sf = 4 ]
+then 
+clear
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlmap.py
+echo "terinstall bos " | lolcat
+fi 
+
+if [ $sf = 5 ] || [ $sf = 5 ]
+then 
+clear
+Pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/siputra12/prank
+cd prank
+pkg install php
+sleep 1
+php wa.php
+echo "terinstall cokkk "|lolcat
+fi
+
+if [ $sf = 6 ] || [ $sf = 6 ]
+then 
+clear
+apt upgrade && apt update
+pkg install git lolcat
+git clone https://github.com/TUANB4DUT/DIR
+cd DIR
+chmod 777 dir.sh
+./dir.sh
+echo "tidak semua website bisa " | lolcat
+fi 
+
+if [ $sf = 7 ] || [ $sf = 7 ]
+then 
+clear
+pkg install python python2 vim figlet curl
+pkg install php 
+pip2 install lolcat 
+pkg install git 
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sleep 1
+sh TUANB4DUT.sh
+fi
+
+if [ $sf = 8 ] || [ $sf = 8 ]
+then 
+clear
+pkg update && pkg upgrade -y
+pkg install figlet
+pkg install php
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone https://github.com/cyberghost69/bima-bagus-jabang-v3
+cd bima-bagus-jabang-v3
+sh bima-bagus-jabang-v3.sh
+fi
+
+if [ $sf = 9 ] || [ $sf = 9 ]
+then
+clear
+apt update && apt upgrade -y
+git clone https://github.com/TU4NN1CKN4M3/TOOLSv2
+cd TOOLSv2
+sh TU4NN1CKN4M3.sh
+fi
+
+if [ $sf = 10 ] || [ $sf = 10 ]
+then
+clear 
+pkg update && pkg upgrade -y
+pkg install python2
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+fi 
+
+if [ $sf = 11 ] || [ $sf = 11 ]
+then
+clear
+pkg install neofetch
+neofetch
+fi
+
+if [ $sf = 12 ] || [ $sf = 12 ]
+then 
+clear
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+cd Nethunter-In-Termux
+chmod +x kalinethunter
+./kalinethunter
+./startkali.sh
+./startkali.sh
+fi
+
+if [ $sf = 13 ] || [ $sf = 13 ]
+then 
+clear
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer/
+cd FHX-Hash-Killer
+python2 FHXHashKiller.py
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+fi
+
+if [ $sf = 14 ] || [ $sf = 14 ]
+then 
+clear
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/Senitopeng/
+Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $sf = 15 ] || [ $sf = 15 ]
+then 
+clear
+Apt install python git
+git clone https://github.com/maldevel/IPGeoLocation.git
+cd IPGeoLocation
+chmod +x ipgeoLocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+python ipgeolocation.py -t http://www.google.com
+fi
+
+if [ $sf = 16 ] || [ $sf = 16 ]
+then 
+clear
+pkg install update
+pkg install git
+pkg install toilet
+pkg install figlet
+pip2 install lolcat
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $sf = 17 ] || [ $sf = 17 ]
+then 
+clear
+pkg install git
+pkg install python
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python/python2 app.py
+fi
+
+if [ $sf = 18 ] || [ $sf = 18 ]
+then 
+clear
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+fi
+
+if [ $sf = 19 ] || [ $sf = 19 ]
+then 
+clear
+apt update
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubu&#8230;
+cd termux-ubuntu
+chmod +x ubuntu.sh
+./ubuntu.sh
+./start.sh
+fi
+
+if [ $sf = 20 ] || [ $sf = 20 ]
+then 
+clear
+pkg install update 
+pkg install upgrade
+pkg install wget
+pkg install php
+wget http://files-store.theprivat.ml/uploads/bom-mall.zip
+unzip bom-mall.zip
+cd bom-mall
+php run.php
+fi
+
+if [ $sf = 21 ] || [ $sf = 21 ]
+then 
+clear
+pkg install python2
+pkg install git
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $sf = 22 ] || [ $sf = 22 ]
+then 
+clear
+apt update && upgrade 
+apt install python2 
+pip2 install urllib3 chardet certifi idna requests 
+apt install openssl curl 
+pkg install libcurl 
+ln -s /sdcard 
+cd sdcard 
+mkdir webdav 
+cd webdav 
+fi
+
+if [ $sf = 23 ] || [ $sf = 23 ]
+then 
+clear
+pkg install python2
+git clone https://github.com/manisso/fsociety
+cd fsociety
+./install.sh
+python2 fsociety.py
+fi
+
+if [ $sf = 24 ] || [ $sf = 24 ]
+then 
+clear
+apt update
+apt install git
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+apt install python2
+chmod +x d-tect.py
+python2 d-tect.py
+fi
+
+if [ $sf = 25 ] || [ $sf = 25 ]
+then 
+clear
+apt update
+apt install python python2
+apt install git
+git clone https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x dog.py
+Python2 dog.py
+fi
+
+if [ $sf = 26 ] || [ $sf = 26 ]
+then 
+clear
+apt install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+/wifi-hacker.sh
+fi
+
+if [ $sf = 27 ] || [ $sf = 27 ]
+then 
+clear
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+python ipgeolocation.py -t IP 
+fi
+
+if [ $sf = 28 ] || [ $sf = 28 ]
+then 
+clear
+pkg install python2
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+python2 torshammer.py
+fi
+
+if [ $sf = 29 ] || [ $sf = 29 ]
+then 
+clear
+git clone https://github.com/aryanrtm/4wsectools
+cd 4wsectools
+chmod 777 tools
+./tools
+fi
+
+if [ $sf = 30 ] || [ $sf = 30 ]
+then 
+clear 
+pkg install python2 
+pkg install git 
+git clone https://github.com/Gameye98/Lazymux 
+cd Lazymux 
+chmod +x lazymux.py 
+python2 lazymux.py 
+fi
+
+if [ $sf = 00 ] || [ $sf = 00 ]
+then
+clear
+exit
+fi
